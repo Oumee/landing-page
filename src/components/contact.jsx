61ae62cf9dd3b0aa99959eb03571670f11716636
Oumee <kaltoumelyaqoubi@gmail.com>
2024-08-20 12:04:21 +0100

    first commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import React from "react";
+
+ 
+export const Contact = (props) => {
+  
+  return (
+    <div>
+      <div id="contact" className="text-center align-item-center">
+        <div className="container">
+        <div className="section-title">
+          <h2>Contact</h2>    
+        </div>
+
+          <div >
+            <div className="contact-item">
+             
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+          <div >
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.twitter : "/"}>
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.youtube : "/"}>
+                      <i className="fa fa-youtube"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+           
+        </div>
+      </div>
+    </div>
+  );
+};
